e31354dfd7e6cd17e4a1dd939faf8179ccd18b63
mohamancer <mohaamer1@gmail.com>
2022-04-23 17:13:54 +0300

    BLD
    Added models which represent the databases

--- iostore/api/admin.py
+++ iostore/api/admin.py
@@ -1,3 +1,9 @@
+from unicodedata import category
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Request)
+admin.site.register(Category)
+admin.site.register(Bid)
--- iostore/iostore/settings.py
+++ iostore/iostore/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'api.User'
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'frontend.apps.FrontendConfig'
+    'frontend.apps.FrontendConfig',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
