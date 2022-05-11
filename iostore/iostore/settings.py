135ce8554bc8a0bc403375b21120a693f4fb4a12
omer-efrati <50967404+omer-efrati@users.noreply.github.com>
2022-05-11 17:13:54 +0300

    MAINT: name of app is enough

--- iostore/iostore/settings.py
+++ iostore/iostore/settings.py
@@ -31,9 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'base.apps.BaseConfig',
-    'feed.apps.FeedConfig',
-    'users.apps.UsersConfig',
+    'base',
+    'feed',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
