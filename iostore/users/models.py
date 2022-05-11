ebeb97c0622633b43e9c60c21733f63e9e1d8b9a
guyj50 <guyj5030@gmail.com>
2022-05-11 15:22:49 +0300

    DEV: added user model

--- iostore/users/models.py
+++ iostore/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15,null=True,blank=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(default='users/images/avatar.svg', null=True, blank=True)
+    rating = models.FloatField(blank=True,null=True)
+    number_of_reviews = models.IntegerField(default=0,blank=True)
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
