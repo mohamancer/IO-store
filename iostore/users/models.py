ced2e9565da7dbf0b1415c4418044e84739a79d9
guyj50 <guyj5030@gmail.com>
2022-05-12 18:50:08 +0300

    DEV: added styling to the website - same as in the tutorial

--- iostore/users/models.py
+++ iostore/users/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(unique=True , max_length=15,null=True,blank=True)
     bio = models.TextField(blank=True)
-    avatar = models.ImageField(default='static/users/images/avatar.svg', null=True, blank=True, upload_to = 'users/static/users/images')
+    avatar = models.ImageField(default='static/users/images/avatar.svg', null=True, upload_to = 'users/static/users/images')
     rating = models.FloatField(blank=True,null=True)
     number_of_reviews = models.IntegerField(default=0,blank=True)
     USERNAME_FIELD = 'username'
