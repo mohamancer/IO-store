70ff4b586cd16962f71557139ff6f7e4ede98def
mohamancer <mohaamer1@gmail.com>
2022-05-12 13:41:56 +0300

    Dev: Added models related to the feed
    Post, Bid and Category

--- iostore/feed/models.py
+++ iostore/feed/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from users import User
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+    
+
+class Post(models.Model):
+    title = models.CharField(max_length = 25)
+    description = models.TextField()
+    post_image = models.ImageField(default='a.png',blank=True)
+    final_bid = models.ForeignKey('Bid' , on_delete=models.CASCADE,null=True,blank=True)
+    host = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    provider = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,null=True)
+    date_of_creation = models.DateTimeField(auto_now_add=True)    
+    bidding_deadline = models.DateTimeField(null=True)
+    active = models.BooleanField(default = True)
+    
+    def __str__(self):
+        return self.title
+
+class Bid(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    offer = models.FloatField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,null=True)
+    date_of_bid = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    def __str__(self):
+        return self.bidder.username + ' has bid ' + str(self.offer) 
