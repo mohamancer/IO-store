1dca34be183174b2d6275ef81a397a52e57e3910
guyj50 <guyj5030@gmail.com>
2022-05-21 17:38:08 +0300

    DEV: basic recommandation system, working!

--- iostore/recommendation_system/update_score_matrix.py
+++ iostore/recommendation_system/update_score_matrix.py
@@ -24,11 +24,11 @@ def init_matrix():
     global matrix_df
     offers_amount = Offer.objects.all().count()
     users_amount = User.objects.all().count()
-    matrix = np.ones((users_amount,offers_amount))
+    matrix = np.ones((users_amount+1,offers_amount+1))
     bid_list = Bid.objects.all()
     for bid in bid_list:
-        bidder_id = bid.bidder.id - 1
-        offer_id = bid.offer.id - 1
+        bidder_id = bid.bidder.id
+        offer_id = bid.offer.id
         if (bidder_id,offer_id) not in s:
             matrix[bidder_id][offer_id] += bid_bonus
             s.add((bidder_id,offer_id))
@@ -37,10 +37,8 @@ def init_matrix():
     users = User.objects.all()
     for user in users:
         fav_offers = Offer.objects.filter(favorites=user)
-        user_id = user.id - 1
         for fav_offer in fav_offers:
-            fav_offer_id = fav_offer.id - 1
-            matrix[user_id][fav_offer_id] += fav_bonus
+            matrix[user.id][fav_offer.id] += fav_bonus
 
     matrix_df = pd.DataFrame(matrix)
     last_updated = timezone.now()
@@ -58,11 +56,9 @@ def add_user_rows_to_matrix_df(new_users):
 
 def add_bids(new_bids):
     for bid in new_bids:
-        bidder_id = bid.bidder.id - 1
-        offer_id = bid.offer.id - 1
-        if (bidder_id,offer_id) not in s:
-            matrix_df[offer_id][bidder_id] += bid_bonus #first [] should hold COLUMN INDEX and second ROW INDEX
-            s.add((bidder_id,offer_id))
+        if (bid.bidder.id,bid.offer.id) not in s:
+            matrix_df[bid.offer.id][bid.bidder.id] += bid_bonus #first [] should hold COLUMN INDEX and second ROW INDEX
+            s.add((bid.bidder.id,bid.offer.id))
 
 def update_users_offers_bids_in_matrix():
     global last_updated
@@ -78,14 +74,12 @@ def update_users_offers_bids_in_matrix():
 
 def add_or_remove_from_fav(user_id,offer_id,is_add):
     global matrix_df
-    user_index = user_id - 1
-    offer_index = offer_id - 1
     update_users_offers_bids_in_matrix()
     try:
         if is_add:
-            matrix_df[offer_index][user_index] += fav_bonus
+            matrix_df[offer_id][user_id] += fav_bonus
         else:
-            matrix_df[offer_index][user_index] -= fav_bonus
+            matrix_df[offer_id][user_id] -= fav_bonus
     except:
         pass
 
@@ -117,9 +111,10 @@ def construct_cos_similar_matrix(): #this can probably be done more efficiently
     global matrix_df
     global co_sim_matrix
     co_sim_matrix = pd.DataFrame(np.ones((len(matrix_df), len(matrix_df))))
-    for i in range(len(matrix_df)):
-        for j in range(len(matrix_df)):
+    for i in range(1,len(matrix_df)):
+        for j in range(1,len(matrix_df)):
             co_sim_matrix[j][i] = 1 - spatial.distance.cosine(matrix_df.iloc[i].tolist(), matrix_df.iloc[j].tolist())
+            #co_sim_matrix[j][i] = 1 - spatial.distance.cosine(matrix_df.iloc[i].tolist()[1:], matrix_df.iloc[j].tolist()[1:]) #this is ignoring dummy user and offer.
 
     #co_sim_matrix = sklearn.metrics.pairwise.cosine_similarity(matrix_df)
     return co_sim_matrix
@@ -152,7 +147,7 @@ def calc_weighted_score(user,offer):
     # i is the user
     numerator = 0.0
     denominator = 0.0
-    for i in range(len(co_sim_matrix)): # go over all people
+    for i in range(1,len(co_sim_matrix)): # go over all users
         if matrix_df[offer][i] != 0:
             numerator += co_sim_matrix[user][i] * matrix_df[offer][i]
             denominator += co_sim_matrix[user][i] # this might have to go inside the if!!!!
@@ -170,8 +165,8 @@ def construct_prediction_matrix():
     global prediction_matrix
     # co_sim_matrix is of same size like matrix_df
     prediction_matrix = pd.DataFrame(np.ones((len(matrix_df), len(matrix_df.iloc[0]))))
-    for i in range(len(prediction_matrix)):
-        for j in range(len(prediction_matrix.iloc[0])):
+    for i in range(1,len(prediction_matrix)):
+        for j in range(1,len(prediction_matrix.iloc[0])):
             # if matrix_df[j][i] != 1:
             #     prediction_matrix[j][i] = matrix_df[j][i]
             # else:
